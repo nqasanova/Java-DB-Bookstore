253ce92af888f438494345ac875d221fbd23038b
Natavan Hasanova <natavanhesenova4@gmail.com>
2023-12-01 23:27:37 +0400

    Establish connection with the database.

--- src/Connectivity/Database_Connection.java
+++ src/Connectivity/Database_Connection.java
@@ -1,2 +1,21 @@
-package src.Connectivity;public class Database_Connection {
+package src.Connectivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class Database_Connection {
+    public static Connection connect() {
+        String url = "jdbc:postgresql://localhost:5432/Bookstore";
+        String user = "postgres";
+        String password = "280315";
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to Database successfully.");
+        } catch (SQLException var5) {
+            System.out.println(var5.getMessage());
+        }
+        return con;
+    }
 }
