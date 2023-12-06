c797f54805aff5d88342977419000ce56281b68d
Natavan Hasanova <natavanhesenova4@gmail.com>
2023-12-06 15:41:24 +0400

    Update Metadata.

--- src/Metadata/Metadata.java
+++ src/Metadata/Metadata.java
@@ -7,58 +7,82 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Metadata {
+public class Metadata extends Database_Connection {
+    public static void printTableNamesandColumns() throws SQLException {
+        Connection connection = connect();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet tables = metaData.getTables(null, null, "%", new String[]{"TABLE"});
+        while (tables.next()) {
+            String tableName = tables.getString("TABLE_NAME");
+            System.out.println(tableName);
+            System.out.println();
+            printColumns(tableName);
+            System.out.println();
+        }
+    }
 
-    public static void main(String[] args) {
-        // Establish a database connection
-        Connection connection = Database_Connection.connect();
+    public static void printColumns(String tableName) throws SQLException {
+        Connection connection = connect();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet columns = metaData.getColumns(null, null, tableName, null);
+        while (columns.next()) {
+            String columnName = columns.getString("COLUMN_NAME");
+            System.out.println(columnName);
+        }
+    }
 
-        try {
-            // Get the DatabaseMetaData object
-            DatabaseMetaData metaData = connection.getMetaData();
-
-            // Print information about the database
-            System.out.println("Database Name: " + metaData.getDatabaseProductName());
-            System.out.println("Database Version: " + metaData.getDatabaseProductVersion());
-
-            // Get information about tables
-            ResultSet tables = metaData.getTables(null, null, null, new String[]{"TABLE"});
-            System.out.println("\nTables:");
-
-            while (tables.next()) {
-                String tableName = tables.getString("TABLE_NAME");
-                System.out.println("\nTable: " + tableName);
-
-                // Get information about columns in the current table
-                ResultSet columns = metaData.getColumns(null, null, tableName, null);
-
-                System.out.println("Columns:");
-                while (columns.next()) {
-                    String columnName = columns.getString("COLUMN_NAME");
-                    String dataType = columns.getString("TYPE_NAME");
-                    int columnSize = columns.getInt("COLUMN_SIZE");
-                    boolean isNullable = (columns.getInt("NULLABLE") == DatabaseMetaData.columnNullable);
-
-                    System.out.println("  Column Name: " + columnName);
-                    System.out.println("    Type: " + dataType);
-                    System.out.println("    Size: " + columnSize);
-                    System.out.println("    Nullable: " + (isNullable ? "Yes" : "No"));
-                    System.out.println("------");
-                }
+    public static void printColumnDetails() throws SQLException {
+        Connection connection = connect();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet tables = metaData.getTables(null, null, "%", new String[]{"TABLE"});
+        while (tables.next()) {
+            String tableName = tables.getString("TABLE_NAME");
+            ResultSet columns = metaData.getColumns(null, null, tableName, null);
+            System.out.println(tableName);
+            System.out.println();
+            while (columns.next()) {
+                String columnName = columns.getString("COLUMN_NAME");
+                String columnType = columns.getString("TYPE_NAME");
+                System.out.println(columnName + " " + columnType);
             }
+            System.out.println();
+        }
+    }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            // Close the connection
-            try {
-                if (connection != null) {
-                    connection.close();
-                    System.out.println("\nConnection closed.");
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+    public static void printPrimaryKeys() throws SQLException {
+        Connection connection = connect();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet tables = metaData.getTables(null, null, "%", new String[]{"TABLE"});
+        while (tables.next()) {
+            String tableName = tables.getString("TABLE_NAME");
+            ResultSet primaryKeys = metaData.getPrimaryKeys(null, null, tableName);
+            System.out.println(tableName);
+            System.out.println();
+            while (primaryKeys.next()) {
+                String primaryKeyColumnName = primaryKeys.getString("COLUMN_NAME");
+                System.out.println(primaryKeyColumnName);
             }
+            System.out.println();
+        }
+    }
+
+    public static void printForeignKeys() throws SQLException {
+        Connection connection = connect();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet tables = metaData.getTables(null, null, "%", new String[]{"TABLE"});
+        while (tables.next()) {
+            String tableName = tables.getString("TABLE_NAME");
+            ResultSet foreignKeys = metaData.getImportedKeys(null, null, tableName);
+            System.out.println(tableName);
+            System.out.println();
+            while (foreignKeys.next()) {
+                String foreign_key_column = foreignKeys.getString("FKCOLUMN_NAME");
+                String referenced_table_name = foreignKeys.getString("PKTABLE_NAME");
+                String referenced_column_name = foreignKeys.getString("PKCOLUMN_NAME");
+                System.out.print(foreign_key_column + " references " + referenced_table_name + "(" + referenced_column_name + ")");
+                System.out.println();
+            }
+            System.out.println();
         }
     }
 }
